c382b89667ff5c1c67ddd6a52c8cc1ea08366ad5
Thomas Bonenfant <tomeinstein18@gmail.com>
2021-03-06 08:59:05 +0100

    updated database dump

--- CoolSurveysDump/dump.sql
+++ CoolSurveysDump/dump.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.22, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
 --
--- Host: localhost    Database: questionnaire_db
+-- Host: localhost    Database: cool_surveys
 -- ------------------------------------------------------
--- Server version	8.0.22
+-- Server version	8.0.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -63,7 +63,7 @@ CREATE TABLE `bad_words` (
 
 LOCK TABLES `bad_words` WRITE;
 /*!40000 ALTER TABLE `bad_words` DISABLE KEYS */;
-INSERT INTO `bad_words` VALUES ('anus'),('arse'),('ass'),('bastard'),('bimbominkia'),('bitch'),('dick'),('dickhead'),('fuck'),('idiot'),('moron'),('shit'),('slut'),('vero'),('whore');
+INSERT INTO `bad_words` VALUES ('anus'),('arse'),('ass'),('bastard'),('bimbominkia'),('bitch'),('branching'),('dick'),('dickhead'),('fuck'),('idiot'),('moron'),('shit'),('slut'),('vero'),('whore');
 /*!40000 ALTER TABLE `bad_words` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -238,4 +238,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-09 22:02:13
+-- Dump completed on 2021-03-06  7:55:54
