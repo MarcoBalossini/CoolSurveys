d3eea203ee83ea470bbda2cb1d8a442f51a7e28e
VeronicaCardigliano <veronica.cardigliano@mail.polimi.it>
2021-03-12 18:29:18 +0100

    int replaced by Integer to assume null value, fixed typos in dump, columns names updated

--- coolSurveysEJB/src/main/java/it/polimi/db2/coolsurveys/entities/BadWords.java
+++ coolSurveysEJB/src/main/java/it/polimi/db2/coolsurveys/entities/BadWords.java
@@ -1,5 +1,17 @@
 package it.polimi.db2.coolsurveys.entities;
 
-public enum BadWords {
-    ANUS, ASS, BASTARD, BITCH, DICK, FUCK, IDIOT, SHIT, SLUT, WHORE, ASSHOLE, DICKHEAD
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "bad_words")
+public class BadWords implements Serializable {
+
+    @Id
+    @Column(name = "word", nullable = false)
+    private String word;
+
 }
