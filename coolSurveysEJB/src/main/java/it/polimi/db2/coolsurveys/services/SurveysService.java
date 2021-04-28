2af996e1534bde512bb5ad2eb162bcf408bd3e10
VeronicaCardigliano <veronica.cardigliano@mail.polimi.it>
2021-04-28 15:25:03 +0200

    SurveysService changed to stateless

--- coolSurveysEJB/src/main/java/it/polimi/db2/coolsurveys/services/SurveysService.java
+++ coolSurveysEJB/src/main/java/it/polimi/db2/coolsurveys/services/SurveysService.java
@@ -1,10 +1,15 @@
 package it.polimi.db2.coolsurveys.services;
 
 import it.polimi.db2.coolsurveys.dao.QuestionnaireDAO;
+import it.polimi.db2.coolsurveys.entities.Option;
+import it.polimi.db2.coolsurveys.entities.Question;
 import it.polimi.db2.coolsurveys.entities.Questionnaire;
 
 import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import java.util.List;
 
+@Stateless
 public class SurveysService implements ISurveysService {
 
     @EJB(name = "it.polimi.db2.coolsurveys.dao/UserDAO")
